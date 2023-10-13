a8272769c2db642735a435ac2b172b82ad9327e1
bernardo.seabra <bernardo.seabra@digitalsys.com.br>
2023-10-13 19:15:51 -0300

    movie

--- src/app/movieDescription/[id]/page.tsx
+++ src/app/movieDescription/[id]/page.tsx
@@ -1,28 +1,38 @@
 import Header from "@/components/Header";
 import styles from "./page.module.css";
 import Image from "next/image";
-import { format } from "date-fns";
-
 interface MovieDescription {
-  title: string;
-  overview: string;
-  poster_path: string;
-  genres: {
+  title?: string;
+  overview?: string;
+  poster_path?: string;
+  genres?: {
     name: string;
   }[];
-  runtime: number;
-  release_date: string;
-  popularity: number;
-  budget: number;
-  revenue: number;
-  status: string;
-  original_language: string;
+  runtime?: number;
+  release_date?: string;
+  popularity?: number;
+  budget?: number;
+  revenue?: number;
+  status?: string;
+  original_language?: string;
+  videos?: {
+    results: {
+      site: string;
+      key: string;
+    }[];
+  };
 }
 
-export default async function movieDescription() {
+export default async function movieDescription({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const movie_id = parseInt(params.id);
+
   async function getMoviesId(movie_id: number) {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movie_id}?language=pt-BR`,
+      `https://api.themoviedb.org/3/movie/${movie_id}?append_to_response=videos&language=pt-BR`,
       {
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
@@ -33,35 +43,46 @@ export default async function movieDescription() {
     return data;
   }
 
-  const data = (await getMoviesId(11)) as MovieDescription;
-  console.log(data);
+  const data = (await getMoviesId(movie_id)) as MovieDescription;
 
-  function formatDataPopularity(dateString: string) {
+  const youtubeVideoKey = data.videos?.results.find(
+    (video) => video.site === "YouTube"
+  )?.key;
+
+  function formatData(dateString?: string) {
+    if (!dateString) return "";
     const [year, month, day] = dateString.split("-");
     const formattedDate = `${day}/${month}/${year}`;
     return formattedDate;
   }
-  const formattedPopularity = formatDataPopularity(data.release_date);
+  const formattedPopularity = formatData(data.release_date);
 
-  const roundedPopularity = Math.floor(data.popularity);
+  const roundedPopularity = data.popularity ? Math.floor(data.popularity) : "?";
 
-  const formattedBudget = data.budget.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-  });
+  const formattedBudget = data.budget
+    ? data.budget.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2,
+      })
+    : "";
 
-  const formattedRevenue = data.revenue.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-  });
+  const formattedRevenue = data.revenue
+    ? data.revenue.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2,
+      })
+    : "";
 
-  const formattedProfit = (data.revenue - data.budget).toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-  });
+  const formattedProfit =
+    data.revenue && data.budget
+      ? (data.revenue - data.budget).toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+          minimumFractionDigits: 2,
+        })
+      : "";
 
   return (
     <body className={styles.main}>
@@ -72,14 +93,17 @@ export default async function movieDescription() {
           <span className={styles.year}>{formattedPopularity}</span>
         </div>
         <div className={styles.descriptionContainer}>
-          <div>
+          <div className={styles.informationMovie}>
             <h3 className={styles.sinopse}>Sinopse</h3>
             <p className={styles.description}>{data.overview}</p>
             <h3 className={styles.sinopse}>Informações</h3>
 
             <div className={styles.information}>
               <h4 className={styles.info}>
-                Situação <p className={styles.infoAdditional}>{data.status}</p>
+                Situação{" "}
+                <p className={styles.infoAdditional}>
+                  {data.status === "Released" ? "Lançado" : "não lançado"}
+                </p>
               </h4>
               <h4 className={styles.info}>
                 Idioma{" "}
@@ -139,7 +163,7 @@ export default async function movieDescription() {
 
             <div className={styles.categoryContainer}>
               <div>
-                {data.genres.map((genre, index) => (
+                {data.genres?.map((genre, index) => (
                   <span key={index} className={styles.category}>
                     {genre.name}
                   </span>
@@ -154,14 +178,27 @@ export default async function movieDescription() {
           </div>
           <div className={styles.imageMovie}>
             <Image
-              src={"/transformers_PNG16.png"}
-              alt={data.title}
+              src={"https://image.tmdb.org/t/p/original" + data.poster_path}
+              alt={data.title ?? ""}
               fill={true}
               objectFit="cover"
             />
           </div>
         </div>
       </section>
+      {youtubeVideoKey ? (
+        <div className={styles.movieYoutube}>
+          <iframe
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${youtubeVideoKey}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ) : null}
     </body>
   );
 }
